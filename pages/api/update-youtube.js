b3096d40958ecea29935d559159a97c23800d5bd
Jake Tigchelaar <jake.arduino@gmail.com>
2021-11-28 17:42:14 -0500

    get current live video

--- pages/api/update-youtube.js
+++ pages/api/update-youtube.js
@@ -2,6 +2,13 @@ import {youtube} from "@googleapis/youtube"
 import Video from '../../models/video'
 import dbConnect from '../../lib/dbConnect'
 
+// this is called by a CRON job on a raspberry pi
+// #!/bin/bash
+// # RUN THIS ON A CRON SCHEDULE
+// # */5 09-11 * * *  => 9-11 am = 180 minutes => every 5 min => 7200 quota (2 calls)
+// # 0 08,12-20 * * *  8am-8pm = 720 minutes => every 1 hour => 2400 quota (2 calls)
+// curl  --request POST --url 'https://first-reformed-church.vercel.app/api/update-youtube' --header 'Authorization: Bearer {{API_SECRET_KEY}}'
+
 export default async function UpdateYoutube(req, res) {
   try {
     if (req.method === 'POST') {
@@ -20,12 +27,24 @@ export default async function UpdateYoutube(req, res) {
           auth: process.env.YOUTUBE_API_KEY
         })
 
-        //download last 5 videos from youtube
-        const resp = await client.search.list({
+        // get current live video
+        let resp = await client.search.list({
           part: 'snippet',
           channelId: process.env.YOUTUBE_CHANNEL_ID,
-          order: 'date'
+          maxResults: 1,
+          type: 'video',
+          eventType: 'live'
         })
+
+        // get last 5 videos
+        if (!resp?.data?.items || resp?.data?.items?.length == 0) {
+          resp = await client.search.list({
+            part: 'snippet',
+            channelId: process.env.YOUTUBE_CHANNEL_ID,
+            type: 'video',
+            order: 'date'
+          })
+        }
 
         if (resp?.data?.items) {
           for (let i = 0; i < resp.data.items.length; i++) {
