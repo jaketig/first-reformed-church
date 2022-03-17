38f4197ff2f8b52714dcea599485d63fb88549b9
Jake Tigchelaar <jake.arduino@gmail.com>
2022-03-16 22:07:15 -0400

    update theme colors

--- components/homepage/MissionStatement.js
+++ components/homepage/MissionStatement.js
@@ -1,9 +1,19 @@
 import {AppBar, Container, Typography} from "@mui/material";
 
 export default function MissionStatement() {
+  const styles = {
+    appbar: {
+      mb: 2
+    },
+    container: {
+      textAlign: "center",
+      my: 2
+    }
+  }
+
   return (
-    <AppBar position={"relative"} color={"secondary"} sx={{mb: 2}}>
-      <Container sx={{textAlign: "center", my: 2}}>
+    <AppBar position={"relative"} color={"secondary"} sx={styles.appbar}>
+      <Container sx={styles.container}>
         <Typography><b>Our Mission:</b> To Encourage, Equip, and Engage members as they follow Jesus</Typography>
       </Container>
     </AppBar>
